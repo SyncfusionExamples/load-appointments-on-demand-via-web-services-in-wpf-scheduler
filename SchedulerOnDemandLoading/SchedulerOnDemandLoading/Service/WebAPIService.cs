6340c7ac9482056b71ff10f64fa7e6804788bb18
Karthik Raja AKR <41435300+KarthikRajaAKR@users.noreply.github.com>
2020-12-22 11:57:58 +0530

    Add files via upload
    I have commited scheduler load on demand sample

---
+++ SchedulerOnDemandLoading/SchedulerOnDemandLoading/Service/WebAPIService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerOnDemandLoading
+{
+    public class WebAPIService
+    {
+        private HttpClient client;
+
+        public WebAPIService()
+        {
+            client = new HttpClient();
+        }
+
+        /// <summary>
+        /// Asynchronously fetching the data from the web API service.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ObservableCollection<SchedulerAppointment>> GetAppointmentsAsync()
+        {
+            var uri = new Uri("https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData");
+            try
+            {
+                var response = await client.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<ObservableCollection<SchedulerAppointment>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+    }
+}
